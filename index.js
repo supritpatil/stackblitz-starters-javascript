41c2bef2889c2c85bfff85c7225c6092dfa5ff2b
supritpatil <118195366+supritpatil@users.noreply.github.com>
2023-06-27 23:05:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+javascript
+function sortStudents(students) {
+   students.sort(function(a,b) {
+  if (a["chemistry marks"] +
+a["biology marks"] > b ["chemistry marks"] + b["biology marks"]) {
+  return -1;
+ } else if (a["chemistry marks"] + a["biology marks"] < b["chemistry marks"] + b["biology marks"]) {
+  return 1;
+ }
+ if (a["biology marks"] > b["biology marks"]) {
+   return -1;
+ } else if (a["biology marks"] < b["biology marks"]) {
+   return 1;
+ }
+ var aDob = new Date(a["date of birth"].split("-").reverse().join("-"));
+ var bDob = new Date(b["date of birth"].split("-").reverse().join("-"));
+ if (aDob > bDob) {
+   return 1;
+ } else if (aDob < bDob) {
+   return -1;
+ }
+   return 0;
+ });
+   return students;
+}
